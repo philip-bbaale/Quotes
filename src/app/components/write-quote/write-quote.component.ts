352c94bd8b40acb901736265462cc04929998c54
Pip Bbaale <namugera.philip@gmail.com>
2020-03-27 20:09:26 +0300

    adding quote writing component

--- src/app/components/write-quote/write-quote.component.ts
+++ src/app/components/write-quote/write-quote.component.ts
@@ -1,4 +1,4 @@
-/*import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Quotes } from '../../classes/quotes';
 
 @Component({
